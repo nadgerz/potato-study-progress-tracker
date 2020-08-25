3a444e7d7a96f43f8762b8c58263a31034ee7a27
Jing-Jing Hu <jing-jing.hu@outlook.com>
2020-08-25 23:00:06 +0200

    Edit user card component

--- frontend/src/components/user-card.vue
+++ frontend/src/components/user-card.vue
@@ -1,9 +1,34 @@
 <script>
 export default {
-    name: 'UserCard'
+  name: 'UserCard',
+  props: ['user'],
+  data() {
+    return {
+      defaultBio: 'This user has no bios.',
+    }
+  },
 }
 </script>
 
 <template lang="pug">
-h2 This is a user-card
+.box
+  h1 {{ user.name }} ({{ user.age }})
+  p Bio: {{ user.bio || defaultBio }}
+  h2 Photos ({{ user.photos.length }})
+
+  .photo(v-for="photo in user.photos")
+    h3 {{ photo.filename }}
+    p(v-if="!photo.likedBy.length")
+      | no likes yet!
+    p(v-else)
+      | {{ photo.likedBy.map(user => user.name).join(', ') }}
 </template>
+
+<style lang="scss" scoped>
+.box {
+  padding: 2rem;
+  border: 1px solid #333;
+  background: #dd0;
+  border-radius: 0.3rem;
+}
+</style>
