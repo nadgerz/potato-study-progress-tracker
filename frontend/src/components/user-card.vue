5d56605d91ff34cbfbc0b2e525a4a62cc142c52c
Jing-Jing Hu <jing-jing.hu@outlook.com>
2020-08-25 22:42:44 +0200

    Create user card component

---
+++ frontend/src/components/user-card.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+    name: 'UserCard'
+}
+</script>
+
+<template lang="pug">
+h2 This is a user-card
+</template>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,19 +1,12 @@
-<template lang="pug">
-  .home
-    img(alt="Vue logo" src="../assets/logo.png")
-    HelloWorld(msg="Welcome to Your Vue.js App")
-    div(v-for="user in users") {{ user.name }} has {{ user.photos.length }} photos
-</template>
-
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/hello-world.vue'
 import axios from 'axios'
+import UserCard from '@/components/user-card.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    UserCard
   },
   data() {
     return {
@@ -27,3 +20,10 @@ export default {
   },
 }
 </script>
+
+<template lang="pug">
+  .home
+    h1 Potato
+    user-card
+    div(v-for="user in users") {{ user.name }} has {{ user.photos.length }} photos
+</template>
